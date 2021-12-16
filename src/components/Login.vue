de880ee0fdf1fd265625fa1447f2910f9c8be309
tuxuwen <1371969653@qq.com>
2021-12-16 15:52:28 +0800

    登录页面初始化

---
+++ src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="login_container">
+        <div class="login_box"></div>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+    background-color: #2b4b6b;
+    height: 100%;
+}
+.login_box {
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 5px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,12 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Router from 'vue-router'
+import Login from '../components/Login.vue'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
-  routes
+export default new Router({
+  routes: [
+    { path: '/', redirect: '/Login' },
+    { path: '/login', component: Login }
+  ]
 })
-
-export default router
