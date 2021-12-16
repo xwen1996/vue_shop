80b34421a3b9ae99a8d1fbe43a1c9e6c602c507c
tuxuwen <1371969653@qq.com>
2021-12-16 19:50:42 +0800

    登录页完成

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,26 +1,112 @@
 <template>
     <div class="login_container">
-        <div class="login_box"></div>
+        <!-- 登录框 -->
+        <div class="login_box">
+            <!-- 登录头像区 -->
+            <div class="avatar_box">
+                <img src="../assets/login_logo.jpg" alt="">
+            </div>
+            <!-- 登录表单区 -->
+            <el-form ref="loginFormRef" :model='loginForm' :rules="loginFormRules" label-width="0px" class="login_form">
+                <!-- 用户名 -->
+                <el-form-item prop="username">
+                    <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+                </el-form-item>
+                <!-- 密码 -->
+                <el-form-item prop="password">
+                    <el-input v-model="loginForm.password" prefix-icon="iconfont icon-3702mima" type='password'></el-input>
+                </el-form-item>
+                <!-- 按钮 -->
+                <el-form-item class="btns">
+                    <el-button type="primary" @click="login">登录</el-button>
+                    <el-button type="info" @click="restLoginForm">重置</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
     </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      // 这是登录表单的数据绑定对象
+      loginForm: {
+        username: 'admin',
+        password: '123456'
+      },
+      // 这是表单的验证规则对象
+      loginFormRules: {
+        // 验证用户名是否合法
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 16, message: '长度在 3 到 16 个字符', trigger: 'blur' }
+        ],
+        // 验证密码是否合法
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 16, message: '长度在 6 到 16 个字符', trigger: 'blur' }]
+      }
+    }
+  },
+  methods: {
+    // 点击重置按钮，重置登录表单
+    restLoginForm () {
+      this.$refs.loginFormRef.resetFields()
+    },
+    // 点击登录按钮，进行登录校验
+    login () {
+      this.$refs.loginFormRef.validate(async valid => {
+        if (!valid) return
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        console.log(res)
+      })
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
 .login_container {
-    background-color: #2b4b6b;
+    background-color: #333333;
     height: 100%;
 }
+//登录框
 .login_box {
     width: 450px;
     height: 300px;
-    background-color: #fff;
+    background-color: #1E1E1E;
     border-radius: 5px;
     position: absolute;
     left: 50%;
     top: 50%;
     transform: translate(-50%,-50%);
+//登录头像区样式
+    .avatar_box{
+        position: absolute;
+        height: 150px;
+        width: 150px;
+        border: 1px solid #eee;
+        border-radius: 50%;
+        box-shadow: 0 0 10px #ddd;
+        left: 50%;
+        transform: translate(-50%,-40%);
+        img {
+            height: 100%;
+            width: 100%;
+            border-radius: 50%;
+        }
+    }
+}
+.login_form{
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+}
+.btns {
+    display: flex;
+    justify-content: flex-end;
 }
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,12 @@
 import Vue from 'vue'
+// eslint-disable-next-line import/no-duplicates
 import { Button } from 'element-ui'
+// eslint-disable-next-line import/no-duplicates
+import { Form, FormItem } from 'element-ui'
+// eslint-disable-next-line import/no-duplicates
+import { Input } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
